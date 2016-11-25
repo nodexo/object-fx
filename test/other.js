b456040d941eae97d2b3253d50d3c616691869f8
Michael Kortstiege <mk@nodexo.de>
2016-11-25 14:47:48 +0100

    add tests for array of objects

---
+++ test/other.js
@@ -0,0 +1,28 @@
+
+'use strict'
+const tap = require('tap')
+const objectFx = require('../index.js')
+
+let flattened = null
+let expanded = null
+
+/**
+ * Array of objects
+ */
+const arrayOfObjects  = [{ a: 1 }, { b: 2 }, { c: 3 }]
+
+flattened = objectFx.flatten(arrayOfObjects)
+tap.strictSame(flattened, {
+    '0.a': 1,
+    '1.b': 2,
+    '2.c': 3
+}, 'Flatten - Array of objects')
+tap.strictSame(objectFx.expand(flattened), arrayOfObjects, 'Expand - Array of objects')
+
+flattened = objectFx.flatten(arrayOfObjects, { ExplicitArrays: true })
+tap.strictSame(flattened, {
+    '[0].a': 1,
+    '[1].b': 2,
+    '[2].c': 3
+}, 'Flatten - Array of objects (explicit arrays)')
+tap.strictSame(objectFx.expand(flattened, { ExplicitArrays: true }), arrayOfObjects, 'Expand - Array of objects (explicit arrays)')
