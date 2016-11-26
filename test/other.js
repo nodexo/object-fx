42250bc83e536e1a8deb17c5a8637f571d782dd1
Michael Kortstiege <mk@nodexo.de>
2016-11-26 05:52:02 +0100

    fix source code formatting [skip ci]

--- test/other.js
+++ test/other.js
@@ -4,25 +4,25 @@ const tap = require('tap')
 const objectFx = require('../index.js')
 
 let flattened = null
-let expanded = null
+// let expanded = null
 
 /**
  * Array of objects
  */
-const arrayOfObjects  = [{ a: 1 }, { b: 2 }, { c: 3 }]
+const arrayOfObjects = [{ a: 1 }, { b: 2 }, { c: 3 }]
 
 flattened = objectFx.flatten(arrayOfObjects)
 tap.strictSame(flattened, {
-    '0.a': 1,
-    '1.b': 2,
-    '2.c': 3
+  '0.a': 1,
+  '1.b': 2,
+  '2.c': 3
 }, 'Flatten - Array of objects')
 tap.strictSame(objectFx.expand(flattened), arrayOfObjects, 'Expand - Array of objects')
 
 flattened = objectFx.flatten(arrayOfObjects, { ExplicitArrays: true })
 tap.strictSame(flattened, {
-    '[0].a': 1,
-    '[1].b': 2,
-    '[2].c': 3
+  '[0].a': 1,
+  '[1].b': 2,
+  '[2].c': 3
 }, 'Flatten - Array of objects (explicit arrays)')
 tap.strictSame(objectFx.expand(flattened, { ExplicitArrays: true }), arrayOfObjects, 'Expand - Array of objects (explicit arrays)')
